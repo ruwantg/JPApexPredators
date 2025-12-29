299f518937b81694f681327e3bdbb1104c4c4e34
Ruwan Thalagaha Gedara <ruwantgs@gmail.com>
2025-12-29 02:57:24 -0500

    Initial Commit

---
+++ JPApexPredators/JPApexPredatorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  JPApexPredatorsApp.swift
+//  JPApexPredators
+//
+//  Created by Ruwan Thalgahage on 2025-12-29.
+//
+
+import SwiftUI
+
+@main
+struct JPApexPredatorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

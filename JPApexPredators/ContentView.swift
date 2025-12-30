d57ed308cff60e042fe132cdd59a9ee7539693ef
Ruwan Thalagaha Gedara <ruwantgs@gmail.com>
2025-12-30 01:27:07 -0500

    feat: add predators list to ContentView

--- JPApexPredators/ContentView.swift
+++ JPApexPredators/ContentView.swift
@@ -12,8 +12,31 @@ struct ContentView: View {
     
     var body: some View {
         List(predators.apexPredators) { predator in
-            Text(predator.name)
+            HStack {
+             // Dinosaur image
+                Image(predator.image)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 100, height: 100)
+                    .shadow(color: .white, radius: 1)
+                
+                VStack(alignment: .leading) {
+                    // Name
+                    Text(predator.name)
+                        .fontWeight(.bold)
+                    
+                    // Type
+                    Text(predator.type.rawValue.capitalized)
+                        .font(.subheadline)
+                        .fontWeight(.semibold)
+                        .padding(.horizontal, 13)
+                        .padding(.vertical, 5)
+                        .background(predator.type.background)
+                        .clipShape(.capsule)
+                }
+            }
         }
+        .preferredColorScheme(.dark)
     }
 }
 
